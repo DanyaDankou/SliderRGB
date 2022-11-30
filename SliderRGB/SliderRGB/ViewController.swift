872a2651e69e66d787fde6171b268ecc6621e41f
DanyaDankou <93947618+DanyaDankou@users.noreply.github.com>
2022-11-30 17:42:21 +0300

    project start

---
+++ SliderRGB/SliderRGB/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SliderRGB
+//
+//  Created by comp on 30.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
