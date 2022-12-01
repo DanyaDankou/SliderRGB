43d1fdde026ddd4e97fe87cc750f224689f72823
DanyaDankou <93947618+DanyaDankou@users.noreply.github.com>
2022-12-01 13:05:01 +0300

    bad try

--- SliderRGB/SliderRGB/ViewController.swift
+++ SliderRGB/SliderRGB/ViewController.swift
@@ -7,12 +7,34 @@
 
 import UIKit
 
+protocol CompareColor {
+    func updateColor (color: UIColor?)
+}
+
 class ViewController: UIViewController {
 
+    var colorBack: Color!
+    @IBOutlet var viewControllerBack: UIView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    
+    @IBAction func goToVC2(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateViewController(withIdentifier: "ViewController2") as?
+                ViewController2 else { return }
+        vc.delegates = self
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
 
-
+extension ViewController: CompareColor {
+    func updateColor(color: UIColor?) {
+        viewControllerBack.backgroundColor = color
+    }
 }
 
